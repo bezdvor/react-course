18ede99adf42da159332e12d0600031fbe8e24f6
alex <alexei.kravchenko@ecocoach.com>
2018-01-31 19:36:00 +0200

    lesson2

--- src/js/components/AddPost.js
+++ src/js/components/AddPost.js
@@ -1,24 +1,15 @@
 import React from 'react';
-import {findDOMNode} from 'react-dom';
 
-import {connect} from 'react-redux';
-import {addPost} from '../actions';
-import Notify from './Notify';
+export default class AddPost extends React.Component {
 
-@connect(null, {addPost})
-export default class AddPost extends React.PureComponent {
-    
     constructor(props) {
         super(props);
-        this.state = {
-            notify: false
-        }
+
         this.handleOnSubmit = this.handleOnSubmit.bind(this);
-        this.unmountNotify = this.unmountNotify.bind(this);
     }
 
-    handleOnSubmit(e) {
-        e.preventDefault();
+    handleOnSubmit(event) {
+        event.preventDefault();
 
         if(this.refs.title.value.trim() !== '' || this.refs.description.value.trim() !== '') {
             let new_post = {
@@ -27,28 +18,17 @@ export default class AddPost extends React.PureComponent {
             };
 
             this.props.addPost(new_post);
-            this.setState({notify: true});
+            
+            this.refs.title.value = '';
+            this.refs.description.value = '';
         }
     };
-
-    componentDidUpdate() {
-        this.refs.title.value = '';
-        this.refs.description.value = '';
-    }
-
-    componentDidMount() {
-        findDOMNode(this.refs.title).focus();
-    }
-
-    unmountNotify() {
-        this.setState({notify: false});
-    }
 
     render() {
         return (
             <div className="add-post">
-                { this.state.notify ? <Notify title={this.refs.title.value} unmount={this.unmountNotify} /> : null }
                 <h3>Add new post</h3>
+                
                 <form onSubmit={this.handleOnSubmit}>
                     <input type="text" ref="title" placeholder="Post title"/>
                     <textarea ref="description" placeholder="Post content"/>
--- src/js/components/Posts.js
+++ src/js/components/Posts.js
@@ -1,16 +1,62 @@
 import React from 'react';
 
 import Post from '../components/Post';
+import AddPost from './AddPost';
 
 export default class Posts extends React.Component {
+
+    // lifecycle methods START!
+    componentWillMount() {
+        console.log("Component Will Mount | Posts.js")
+    }
+
+    componentDidMount() {
+        console.log("Component Did Mount | Posts.js")
+    }
+
+    componentWillUpdate() {
+        console.log("Component Will Update | Posts.js");
+    };
+
+    shouldComponentUpdate() {
+        console.log("Should Component Update | Posts.js");
+        return true;
+    };
+
+    componentDidUpdate() {
+        console.log("Component Did Update | Posts.js");
+    };
+
+    componentWillUnmount() {
+        console.log("componentWillUnmount | Posts.js");
+    }
+
+    componentWillReceiveProps(nextProps) {
+        console.log("Component Will Receive Props | Posts.js", nextProps);
+    }
+    //lifecycle methods END!
+
+    // Это не все функции жизненного цикла, остальные вы можете узнать 
+    // посмотрев в документацию реакта (https://reactjs.org) 
 
     render() {
         return (
             <section className="posts-container">
+                <AddPost addPost={this.props.addPost} />
                 <div className="items">
-                    <Post data={ {title: 'react.'} } />
+                    {this.props.posts.map((item, index) => {
+                        // Тут мы перебираем функцией .map() каждый объект из массива переданого в этот компонент и передаем
+                        // каждый из них в новый компонент Post (экземпляр класса), который создается при каждой итерации функции map()
+
+                        // Так-же, мы передаем свойство "key", оно необходимо ядру реакта для индетификации элементов которые
+                        // созданы спомощью итерационных функций, в остальных случаях это делать нет необходимости.
+                        return (
+                            <Post data={item} key={index} />
+                        )
+                    } )}
                 </div>
             </section>
         )
     }
 }
+
